b68d41453cafad6b925a6bf0704ad4c7615e2721
BuildTools <unconfigured@null.spigotmc.org>
2019-10-14 17:15:55 +0800

    0.1.1.01

--- testsystem/src/testsystem/givequestion.java
+++ testsystem/src/testsystem/givequestion.java
@@ -2,14 +2,21 @@ package testsystem;
 
 public class givequestion {
 	qusetionlist list;
+	int num;
 	
 	public givequestion(qusetionlist qlist){
 		list = qlist;
+		num = 1;
 	}
 	
 	public qusetion nextqusetion(){
+		num = num +1;
+		return list.getqusetion(num - 1);
 		
-		
+	}
+	
+	public int getnum() {
+		return num;
 	}
 
 }
